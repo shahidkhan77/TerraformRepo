aea8aaed1a51f7175fc8219d1fa68d756577477c
shahidkhan77 <shahidkhan77@users.noreply.github.com>
2022-01-07 16:10:49 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+	required_version = ">=0.12.13"
+	}
+	
+provider "aws" {
+        region = "us-west-2"
+        access_key = "AKIAWBSM54OB4GIUYFQY"
+        secret_key = "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"
+}
+
+#Aws s3bucket
+resource "aws_s3_bucket" "terraformsimplestorage3bucket" {
+        bucket = "terraformsimplestorage3bucket"
+        acl = "private"
+
+        tags = {
+                Name = "terraformsimplestorage3bucket"
+                Environment = "OPS"
+        }
+
+}
+
